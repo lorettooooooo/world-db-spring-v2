6cbd3857f51ce1bae526dc32db2dce88f9ce31fc
Lorenzo Carchedi <lorenzocarchedi1996@gmail.com>
2021-04-29 15:25:10 +0200

    added errors

--- src/main/java/it/objectmethod/worlddb/controller/HomeRestController.java
+++ src/main/java/it/objectmethod/worlddb/controller/HomeRestController.java
@@ -1,8 +1,9 @@
 package it.objectmethod.worlddb.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,30 +25,80 @@ public class HomeRestController {
 	@Autowired
 	IDaoCountry iDaoCountry;
 
-	@PostMapping("/cityInfo")
-	public List<City> citySearch(@RequestParam("cityName") String cityName) {
+	@PostMapping("/login")
+	public String login(@RequestParam("username") String username, HttpServletResponse response) {
+
+		if (username.isBlank()) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		}
+		return username;
+	}
+
+	@GetMapping("/cityInfo")
+	public List<City> citySearch(@RequestParam("cityName") String cityName, HttpServletResponse response) {
 		List<City> citiesList = null;
-		citiesList = iDaoCity.getCityByName(cityName);
+		if (cityName == null) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		} else {
+			citiesList = iDaoCity.getCityByName(cityName);
+			if (citiesList.size() == 0) {
+				response.setStatus(204);
+			}
+		}
 		return citiesList;
 	}
 
-	@PostMapping("/countryInfo")
+	@GetMapping("/countryInfo")
 	public List<Country> countrySearch(@RequestParam("countryName") String countryName,
-			@RequestParam("continentName") String continentName) {
+			@RequestParam("continentName") String continentName, HttpServletResponse response) {
 		List<Country> countriesList = null;
-		if (countryName.isBlank())
+		if (countryName.isBlank()) {
 			countryName = "";
-		if (continentName.isBlank())
+		}
+		if (continentName.isBlank()) {
 			continentName = "";
+		}
 		countriesList = iDaoCountry.getCountriesByNameAndContinent('%' + countryName + '%', '%' + continentName + '%');
+		if (countriesList.size() == 0) {
+			response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+		}
 		return countriesList;
 	}
 
-	@GetMapping("/{country.code}/cities")
-	public List<City> searchByCountry(@PathVariable("country.code") String selectedCountryCode, HttpSession session) {
-
-		List<City> citiesList = iDaoCity.getCitiesByCountryCode(selectedCountryCode);
-		session.setAttribute("selectedCountryCode", selectedCountryCode);
+	@GetMapping("/{countryCode}/cities")
+	public List<City> searchByCountry(@PathVariable("countryCode") String selectedCountryCode,
+			HttpServletResponse response) {
+		List<City> citiesList = null;
+		if (selectedCountryCode == null) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		} else {
+			citiesList = iDaoCity.getCitiesByCountryCode(selectedCountryCode);
+			if (citiesList.size() == 0) {
+				response.setStatus(204);
+			}
+		}
 		return citiesList;
+	}
+
+	@GetMapping("/searchByContinents")
+	public List<String> searchByContinents() {
+		List<String> continentsList = new ArrayList<String>();
+		continentsList = iDaoCountry.getContinents();
+		return continentsList;
+	}
+
+	@GetMapping("/{selectedContinent}/countries")
+	public List<Country> searchByContinents(@PathVariable("selectedContinent") String selectedContinent,
+			HttpServletResponse response) {
+		List<Country> countriesList = null;
+		if (selectedContinent == null) {
+			response.setStatus(400);
+		} else {
+			countriesList = iDaoCountry.getCountriesByContinent(selectedContinent);
+			if (countriesList.size() == 0) {
+				response.setStatus(204);
+			}
+		}
+		return countriesList;
 	}
 }
